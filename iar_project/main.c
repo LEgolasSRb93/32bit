e4222c2644c0439c518d902fd60ced266e540797
Lazar Cakovic <lazar.cakovic@novelic.com>
2018-08-17 16:27:20 +0200

    MOD: modified indent in main.c

--- iar_project/main.c
+++ iar_project/main.c
@@ -22,175 +22,127 @@ int main()
     /**
     * Defines
     */
-    /**
-    * FIR variables
-    */
     FILE *fp;
     float32_t filter[FILTER_SIZE];
     float32_t samples[SAMPLES_SIZE];
     float32_t output[SAMPLES_SIZE];
 
-#if FIR
+    #if FIR
     arm_fir_instance_f32 S;
     uint32_t i;
     uint32_t blockSize = BLOCK_SIZE;
-    uint32_t numBlocks = SAMPLES_SIZE/BLOCK_SIZE; 
-#else
+    uint32_t numBlocks = SAMPLES_SIZE/BLOCK_SIZE;
+    #else
     /**
     *	fast fft
     */
     arm_rfft_fast_instance_f32 FFT;
-    float32_t meanSignal = 0;  
-    
+    float32_t meanSignal = 0;
+
     uint32_t fftSize = 512;
     uint32_t ifftFlag = 0;
     uint32_t doBitReverse = 1;
-#endif
-    
+    #endif
 
 
-#if FIR
+
+    #if FIR
     /**
     * Reading filter samples to buffer
     */
     fp = fopen("C:\\Users\\lazar\\Documents\\32bit\\samples_float.bin", "rb");
     if(fp == NULL)
     {
-      printf("Could not open file.\n");
-      return 1;
+        printf("Could not open file.\n");
+        return 1;
     }
     fread(samples, sizeof(samples), sizeof(samples)/sizeof(float32_t), fp);
     fclose(fp);
-    
+
     /**
     * Reading filter samples to buffer
     */
     fp = fopen("C:\\Users\\lazar\\Documents\\32bit\\filterNorm_float.bin", "rb");
     if(fp == NULL)
     {
-      printf("Could not open file.\n");
-      return 1;
+        printf("Could not open file.\n");
+        return 1;
     }
     fread(filter, sizeof(filter), sizeof(filter)/sizeof(float32_t), fp);
     fclose(fp);
-    
+
     printf("Reading data from files finished...\r\n");
 
-     /**
+    /**
     * Perform filtering function from arms library
     */
     arm_fir_init_f32(&S, FILTER_SIZE, (float32_t *)&filter[0], &firStateF32[0], blockSize);
-    
+
     for( i = 0; i < numBlocks; i++)
     {
-      arm_fir_f32(&S, samples + (i*blockSize), output + (i*blockSize), blockSize);
+        arm_fir_f32(&S, samples + (i*blockSize), output + (i*blockSize), blockSize);
     }
-    
+
     printf("Filtering with CMSIS filter function finished...\r\n");
- 
-#else
+
+    #else
     /**
     * Reading filter samples to buffer
     */
     fp = fopen("C:\\Users\\lazar\\Documents\\32bit\\samples_float.bin", "rb");
     if(fp == NULL)
     {
-      printf("Could not open file.\n");
-      return 1;
+        printf("Could not open file.\n");
+        return 1;
     }
     for(int i = 0; i < SAMPLES_SIZE; i+=2)
     {
-	fread(&samples[i], sizeof(float32_t), 1, fp);
-	samples[i+1] = 0;
+        fread(&samples[i], sizeof(float32_t), 1, fp);
+        samples[i+1] = 0;
     }
     fclose(fp);
-    
+
     /**
     * Reading filter samples to buffer
     */
     fp = fopen("C:\\Users\\lazar\\Documents\\32bit\\filterPAD_float.bin", "rb");
     if(fp == NULL)
     {
-      printf("Could not open file.\n");
-      return 1;
+        printf("Could not open file.\n");
+        return 1;
     }
     for(int i = 0; i < SAMPLES_SIZE; i+=2)
     {
-	fread(&filter[i], sizeof(float32_t), 1, fp);
-	filter[i+1] = 0;
+        fread(&filter[i], sizeof(float32_t), 1, fp);
+        filter[i+1] = 0;
     }
     fclose(fp);
-    
+
     printf("Reading data from files finished...\r\n");
-    
+
     /* Process the data through the CFFT/CIFFT module */
     arm_cfft_f32(&arm_cfft_sR_f32_len256, samples, ifftFlag, doBitReverse);
-    
-    
-//    /* Process the data through the Complex Magnitude Module for
-//    calculating the magnitude at each bin */
-//    arm_cmplx_mag_f32(testInput_f32_10khz, testOutput, fftSize);
-//    /* Calculates maxValue and returns corresponding BIN value */
-//    arm_max_f32(testOutput, fftSize, &maxValue, &testIndex);
-    /* Process the data through the CFFT/CIFFT module */
+
     arm_cfft_f32(&arm_cfft_sR_f32_len256, filter, ifftFlag, doBitReverse);
-    
-    /**
-    * Put results in file
-    */
-    fp = fopen("C:\\Users\\lazar\\Documents\\32bit\\filterFFT_float.bin", "wb");
-    if(fp == NULL)
-    {
-      printf("Could not open file.\n");
-      return 1;
-    }
-    //fwrite(output, sizeof(output), sizeof(output)/sizeof(float32_t), fp);
-    for(int i = 0; i < NFFT; i+=2)
-    	fwrite(&filter[i], sizeof(float32_t), 1, fp);
-    fclose(fp);
-    printf("Writing filter FFT to the file finished...\r\n");
-    
-//    /* Initialize FFT instance */
-//     arm_rfft_fast_init_f32(&FFT, NFFT);
-//         
-//    // SAMPLES FFT
-//    // Find and subtract mean value
-//    arm_mean_f32(samples, NSig, &meanSignal);
-//    arm_offset_f32(samples, -meanSignal, samples, NSig);
-//    
-//    // Calculate FFT
-//    arm_rfft_fast_f32(&FFT, samples, samples, 0);
-//    arm_abs_f32(samples, samples, NFFT);
-//    
-//    // FILTER FFT
-//    // Find and subtract mean value
-//    arm_mean_f32(filter, 512, &meanSignal);
-//    arm_offset_f32(filter, -meanSignal, filter, 512);
-//    
-//    // Calculate FFT
-//    arm_rfft_fast_f32(&FFT, filter, filter, 0);
-//    arm_abs_f32(filter, filter, NFFT);
-    
+
     printf("Calculating FFT finished...\r\n");
-    
-    
+
+
     for(int i = 0; i < NFFT; i+=2)
     {
-	//output[i/2] = sqrt(pow(samples[i] * filter[i] - samples[i+1] * filter[i+1], 2) + pow(samples[i] * filter[i+1] + samples[i+1] * filter[i], 2));
-	output[i] = samples[i] * filter[i] - samples[i+1] * filter[i+1];
-	output[i+1] = samples[i] * filter[i+1] + samples[i+1] * filter[i];
+        //output[i/2] = sqrt(pow(samples[i] * filter[i] - samples[i+1] * filter[i+1], 2) + pow(samples[i] * filter[i+1] + samples[i+1] * filter[i], 2));
+        output[i] = samples[i] * filter[i] - samples[i+1] * filter[i+1];
+        output[i+1] = samples[i] * filter[i+1] + samples[i+1] * filter[i];
     }
-    
+
     printf("Multiplication finished...\r\n");
-    
+
     // Calculate IFFT
-//    arm_rfft_fast_f32(&FFT, output, output, 1);
-//    arm_abs_f32(output, output, NFFT);    
     arm_cfft_f32(&arm_cfft_sR_f32_len256, output, 1, doBitReverse);
-    
+
     printf("Calculating IFFT finished...\r\n");
-    
-#endif
+
+    #endif
 
     /**
     * Put results in file
@@ -198,12 +150,12 @@ int main()
     fp = fopen("C:\\Users\\lazar\\Documents\\32bit\\output_float.bin", "wb");
     if(fp == NULL)
     {
-      printf("Could not open file.\n");
-      return 1;
+        printf("Could not open file.\n");
+        return 1;
     }
     //fwrite(output, sizeof(output), sizeof(output)/sizeof(float32_t), fp);
     for(int i = 0; i < NFFT; i+=2)
-    	fwrite(&output[i], sizeof(float32_t), 1, fp);
+        fwrite(&output[i], sizeof(float32_t), 1, fp);
     fclose(fp);
     printf("Writing output to the file finished...\r\n");
 
